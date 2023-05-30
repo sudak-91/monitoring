684aca94bf757ee776206a251844087a96cd7a98
sudak-91 <sudalin.kms@gmail.com>
2023-05-30 15:56:44 +0300

    Change Update method. Add createMainNodeDiv and createNodeUnit methods

--- internal/pkg/screens/node_browser.go
+++ internal/pkg/screens/node_browser.go
@@ -11,7 +11,6 @@ import (
 	message "github.com/sudak-91/monitoring/pkg/message"
 	command "github.com/sudak-91/monitoring/pkg/message/command"
 	update "github.com/sudak-91/monitoring/pkg/message/update"
-	"github.com/sudak-91/wasmhtml"
 	"github.com/sudak-91/wasmhtml/element"
 )
 
@@ -75,52 +74,23 @@ func (n *NodeBrowser) Update(data any) {
 		)
 		log.Println("[MainScreen]Update|OPCNodes")
 		if len(upd.Nodes.OrganizesNode) != 0 {
-			organizeNodeDiv = n.NodeTree.AddDiv()
-			organizeNodeDiv.SetID("organizeNode")
-			n.DOMModel[organizeNodeDiv.GetID()] = organizeNodeDiv
+			organizeNodeDiv = n.createMainNodeDiv("organizeNode")
 		}
 
 		if len(upd.Nodes.ComponentNode) != 0 {
-			componentNodeDiv = n.NodeTree.AddDiv()
-			componentNodeDiv.SetID("componentNode")
-			n.DOMModel[componentNodeDiv.GetID()] = componentNodeDiv
+			componentNodeDiv = n.createMainNodeDiv("componentNode")
 		}
 		if len(upd.Nodes.PropertyNode) != 0 {
-			propertyNodeDiv = n.NodeTree.AddDiv()
-			propertyNodeDiv.SetID("propertyNode")
-			n.DOMModel[propertyNodeDiv.GetID()] = propertyNodeDiv
+			propertyNodeDiv = n.createMainNodeDiv("propertyNode")
 		}
 		for _, v := range upd.Nodes.OrganizesNode {
-
-			elem := unit.NewNodeUnit(organizeNodeDiv)
-			switch v.NodeType {
-			case 0:
-				elem.AddClass("foldernode")
-			default:
-				elem.AddClass("datanode")
-			}
-			elem.AddTitle(v.Name)
-			elem.SetAttributes(v.Name, v.Namespace, v.IID, "")
-			elem.AddEventListener(js.FuncOf(n.GetValue))
-			n.DOMModel[v.Name] = elem.GetParentDiv()
+			n.createNodeUnit(organizeNodeDiv, v)
 		}
 		for _, v := range upd.Nodes.ComponentNode {
-			l := componentNodeDiv.AddLi()
-			l.SetID(v.Name)
-			l.SetInnerHtml(v.Name)
-			wasmhtml.SetAttribute(l.Object, "opcid", v.IID)
-			wasmhtml.SetAttribute(l.Object, "opcns", v.Namespace)
-			wasmhtml.AddClickEventListenr(l.Object, js.FuncOf(n.GetValue))
-			n.DOMModel[v.Name] = l
+			n.createNodeUnit(componentNodeDiv, v)
 		}
 		for _, v := range upd.Nodes.PropertyNode {
-			l := propertyNodeDiv.AddLi()
-			l.SetID(v.Name)
-			l.SetInnerHtml(v.Name)
-			wasmhtml.SetAttribute(l.Object, "opcid", v.IID)
-			wasmhtml.SetAttribute(l.Object, "opcns", v.Namespace)
-			wasmhtml.AddClickEventListenr(l.Object, js.FuncOf(n.GetValue))
-			n.DOMModel[v.Name] = l
+			n.createNodeUnit(propertyNodeDiv, v)
 		}
 		n.Parent.Generate()
 		return
@@ -136,47 +106,13 @@ func (n *NodeBrowser) Update(data any) {
 			return
 		}
 		for _, v := range upd.Nodes.OrganizesNode {
-
-			node := unit.NewNodeUnit(parent)
-			log.Println(v.NodeType)
-			switch v.NodeType {
-			case 0:
-				node.AddClass("foldernode")
-			default:
-				node.AddClass("datanode")
-			}
-			node.AddTitle(v.Name)
-			node.SetAttributes(v.Name, v.Namespace, v.IID, v.SID)
-			node.AddEventListener(js.FuncOf(n.GetValue))
-			n.DOMModel[v.Name] = node.GetParentDiv()
+			n.createNodeUnit(parent, v)
 		}
 		for _, v := range upd.Nodes.ComponentNode {
-			node := unit.NewNodeUnit(parent)
-			log.Println(v.NodeType)
-			switch v.NodeType {
-			case 0:
-				node.AddClass("foldernode")
-			default:
-				node.AddClass("datanode")
-			}
-			node.AddTitle(v.Name)
-			node.SetAttributes(v.Name, v.Namespace, v.IID, v.SID)
-			node.AddEventListener(js.FuncOf(n.GetValue))
-			n.DOMModel[v.Name] = node.GetParentDiv()
+			n.createNodeUnit(parent, v)
 		}
 		for _, v := range upd.Nodes.PropertyNode {
-			node := unit.NewNodeUnit(parent)
-			log.Println(v.NodeType)
-			switch v.NodeType {
-			case 0:
-				node.AddClass("foldernode")
-			default:
-				node.AddClass("datanode")
-			}
-			node.AddTitle(v.Name)
-			node.SetAttributes(v.Name, v.Namespace, v.IID, v.SID)
-			node.AddEventListener(js.FuncOf(n.GetValue))
-			n.DOMModel[v.Name] = node.GetParentDiv()
+			n.createNodeUnit(parent, v)
 		}
 		parent.Generate()
 		return
@@ -213,4 +149,25 @@ func (n *NodeBrowser) GetValue(this js.Value, args []js.Value) any {
 	}
 	log.Printf("namespace = %d, id = %d", cmd.GetSubNode.Namespace, cmd.GetSubNode.IID)
 	return nil
+}
+
+func (n *NodeBrowser) createMainNodeDiv(id string) *element.Div {
+	mainNode := n.NodeTree.AddDiv()
+	mainNode.SetID(id)
+	n.DOMModel[mainNode.GetID()] = mainNode
+	return mainNode
+}
+
+func (n *NodeBrowser) createNodeUnit(parentNode *element.Div, v update.NodeDef) {
+	elem := unit.NewNodeUnit(parentNode)
+	switch v.NodeType {
+	case 0:
+		elem.AddClass("foldernode")
+	default:
+		elem.AddClass("datanode")
+	}
+	elem.AddTitle(v.Name)
+	elem.SetAttributes(v.Name, v.Namespace, v.IID, v.SID)
+	elem.AddEventListener(js.FuncOf(n.GetValue))
+	n.DOMModel[v.Name] = elem.GetParentDiv()
 }
